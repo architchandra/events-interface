94f1c486665ad5f062e15770e0b5efb3cca91ad0
Archit Chandra <architchandra@gmail.com>
2019-06-01 18:25:25 +0530

    Rudimentary desktop layout

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function()  {
+
+  $('#may-fifth-elephant-2019').on('click', function()  {
+    $('.event-details-container[data-event-id="may-fifth-elephant-2019"]').appendTo('.sidebar');
+    $('.event-details-container[data-event-id="may-fifth-elephant-2019"]').show();
+  });
+
+});
