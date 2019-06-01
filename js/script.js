f2653a3976b9571fe78e46c1628c13acf998d284
Archit Chandra <architchandra@gmail.com>
2019-06-01 19:13:48 +0530

    Logic to display event details and to reset everything when the window is resized

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 $(function()  {
 
+  /* Reset if the window is resized */
+  $(window).resize(function()  {
+    $('.event-details-container').hide();
+  });
+
+/*   Show event details when the correspinding event is clicked - different logic for different viewport sizes */
   $('#may-fifth-elephant-2019').on('click', function()  {
-    $('.event-details-container[data-event-id="may-fifth-elephant-2019"]').appendTo('.sidebar');
+    $('.event-details-container').hide();
+    if($(window).width() > 767)  {
+      $('.event-details-container[data-event-id="may-fifth-elephant-2019"]').appendTo('.sidebar');
+    }  else  {
+      $('.event-details-container[data-event-id="may-fifth-elephant-2019"]').insertAfter('.calendar-mobile');
+    }
     $('.event-details-container[data-event-id="may-fifth-elephant-2019"]').show();
   });
 
