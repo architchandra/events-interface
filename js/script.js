9be62355481ee5035e6b8b8435c59b99a92789f3
Archit Chandra <architchandra@gmail.com>
2019-06-01 20:10:33 +0530

    Add event details for single-event and fix issues in its working along with small style changes

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(function()  {
 /*   Show event details when the correspinding event is clicked - different logic for different viewport sizes */
   $('#may-fifth-elephant-2019').on('click', function()  {
     $('.event-details-container').hide();
-    if($(window).width() > 767)  {
+    if($(window).width() > 991)  {
       $('.event-details-container[data-event-id="may-fifth-elephant-2019"]').appendTo('.sidebar');
     }  else  {
       $('.event-details-container[data-event-id="may-fifth-elephant-2019"]').insertAfter('.calendar-mobile');
@@ -17,6 +17,19 @@ $(function()  {
       }, 500);
     }
     $('.event-details-container[data-event-id="may-fifth-elephant-2019"]').show();
+  });
+
+  $('#may-ctotalk-with-walmart-labs').on('click', function()  {
+    $('.event-details-container').hide();
+    if($(window).width() > 767)  {
+      $('.event-details-container[data-event-id="may-ctotalk-with-walmart-labs"]').appendTo('.sidebar');
+    }  else  {
+      $('.event-details-container[data-event-id="may-ctotalk-with-walmart-labs"]').insertAfter('.calendar-mobile');
+      $('html, body').animate({
+        scrollTop: $('#calendar-mobile').offset().top + $('#calendar-mobile').height()
+      }, 500);
+    }
+    $('.event-details-container[data-event-id="may-ctotalk-with-walmart-labs"]').show();
   });
 
 });
